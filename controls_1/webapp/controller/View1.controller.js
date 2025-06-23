62c7a95a33ac96a66a14d421212b81f7aaea7faa
Deepraj Debroy <deeprajdebroy1234@gmail.com>
2025-06-23 14:16:38 +0000

    All codes

--- controls_1/webapp/controller/View1.controller.js
+++ controls_1/webapp/controller/View1.controller.js
@@ -5,6 +5,38 @@ sap.ui.define([
 
     return Controller.extend("sap.co.controls1.controller.View1", {
         onInit() {
+            const data = {
+                products: [
+                  { name: "Laptop", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop1", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug1", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack1", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop1", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug1", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack1", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop1", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug1", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack1", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop", category: "Electronics", price: "1200 USD" },
+                  { name: "Coffee Mug", category: "Kitchen", price: "15 USD" },
+                  { name: "Backpack", category: "Accessories", price: "45 USD" },
+                  { name: "Laptop1", category: "Electronics4", price: "1200 USD" },
+                  { name: "Coffee Mug1", category: "Kitchen3", price: "15 USD" },
+                  { name: "Backpack1", category: "Accessories2", price: "45 USD" }
+                ],
+                selectedProduct: {}
+              };
+            
+              const oModel = new sap.ui.model.json.JSONModel(data);
+              this.getView().setModel(oModel, "son");
         },
 
         onPressNavToMaster2: function(oEvent) {
@@ -47,7 +79,14 @@ sap.ui.define([
                 this.oRouter.getTargets().display("TargetVBox")
             }
                 
-        }
+        },
+
+        onSelectItem: function (oEvent) {
+            const oContext = oEvent.getParameter("listItem").getBindingContext("son").getProperty();
+            
+            const oModel = this.getView().getModel("son");
+            oModel.setProperty("/selectedProduct", oContext);
+          }
 
     });
 });
